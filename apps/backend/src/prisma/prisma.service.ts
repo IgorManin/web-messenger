017e684938053f5d0e2180c7d87c70ec7dd9534c
IgorManin <106236581+IgorManin@users.noreply.github.com>
2025-12-09 20:34:55 +0300

    feat(api): integrate Prisma and add initial database setup

---
+++ apps/backend/src/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// путь зависит от того, что мы указали в output в schema.prisma
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+import pg from 'pg';
+
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
+    constructor() {
+        const pool = new pg.Pool({
+            connectionString: process.env.DATABASE_URL,
+        });
+
+        const adapter = new PrismaPg(pool);
+
+        super({ adapter });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
