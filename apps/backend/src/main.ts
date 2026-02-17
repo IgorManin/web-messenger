91d06508b7ba2122ae845f8c86751121e44d3827
Igor Manin <106236581+IgorManin@users.noreply.github.com>
2026-02-17 20:36:35 +0300

    feat(auth): добавить login/refresh/logout и refreshTokenHash (#9)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import * as dotenv from 'dotenv';
 import {ValidationPipe} from "@nestjs/common";
+import cookieParser from 'cookie-parser'
+
 
 dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    app.enableCors({
+        origin: process.env.ALLOWED_ORIGINS?.split(',').map((s) => s.trim()) ?? true,
+        credentials: true,
+    })
+
+    app.use(cookieParser())
 
     app.useGlobalPipes(
         new ValidationPipe({
